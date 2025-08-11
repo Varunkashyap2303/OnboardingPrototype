1fe90641ec0c448142c84a7e2f6d55d99c2ccfad
WenLiu33 <tliu0075@student.monash.edu>
2025-08-11 10:29:19 +1000

    feat: add search bar in map

--- src/components/parkingMap.jsx
+++ src/components/parkingMap.jsx
@@ -1,8 +1,11 @@
-// 'use client';
-import React, { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import "@maptiler/geocoding-control/style.css";
+import { GeocodingControl } from "@maptiler/geocoding-control/leaflet";
+
+const MAPTILER_KEY = process.env.NEXT_PUBLIC_MAPTILER_KEY;
 
 // Mock parking spot data
 const mockSpots = [
@@ -25,6 +28,42 @@ const customIcon = (color) =>
     popupAnchor: [0, -32],
   });
 
+function Geocoder() {
+  const map = useMap();
+  useEffect(() => {
+    const control = new GeocodingControl({
+      apiKey: MAPTILER_KEY,
+      position: "topright",
+      placeholder: "Search address...",
+      marker: true,
+      showResultsWhileTyping: true,
+    });
+    map.addControl(control);
+    return () => map.removeControl(control);
+  }, [map]);
+  return null;
+}
+
+function ZoomBottomLeft() {
+  const map = useMap();
+  useEffect(() => {
+    const z = L.control.zoom({ position: "bottomleft" });
+    z.addTo(map);
+    return () => z.remove();
+  }, [map]);
+  return null;
+}
+
+function FixResizeOnce() {
+  const map = useMap();
+  useEffect(() => {
+    // let layout settle, then recompute sizes
+    const id = setTimeout(() => map.invalidateSize(), 0);
+    return () => clearTimeout(id);
+  }, [map]);
+  return null;
+}
+
 const ParkingMap = () => {
   return (
     <div className="h-full w-full z-0">
@@ -34,12 +73,16 @@ const ParkingMap = () => {
         style={{ height: "100%", width: "100%" }}
         scrollWheelZoom={false}
         className="z-0"
+        zoomControl={false}
       >
+        <FixResizeOnce />
         <TileLayer
           attribution="&copy; OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           className="z-0"
         />
+        <ZoomBottomLeft />
+        <Geocoder />
 
         {mockSpots.map((spot) => (
           <Marker
